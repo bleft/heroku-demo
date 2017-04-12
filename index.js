84f742be41b8d48d8ac9f6ae20a508fd3af83fa7
Sven Bautz <sven.bautz@krick.com>
2017-04-12 16:57:32 +0200

    add demo route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
@@ -16,6 +17,14 @@ app.get('/', function(request, response) {
 
 app.get('/cool', function(request, response) {
   response.send(cool());
+});
+
+app.get('/demo', function(request, response) {
+    response.type('json');
+
+    let entries = {"Pullover", "Mützen", "Hosen", "Schuhe"};
+    response.status(200).send(entries);
+
 });
 
 app.listen(app.get('port'), function() {
