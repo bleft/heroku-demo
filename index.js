acfcb14637549069c8d74a1e7e7a16cef8a1e737
Sven Bautz <sven.bautz@krick.com>
2017-06-19 16:19:07 +0200

    mongo connect

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
 
 app.set('port', (process.env.PORT || 5000));
  
@@ -22,7 +24,13 @@ app.get('/demo', function(request, response) {
     response.type('json');
 
     var url = process.env.PROD_MONGODB;
-    response.status(200).send(url);
+    MongoClient.connect(url, function(err, db){
+        assert.equal(null, err);
+        var version = db.VersionInfo.findOne();
+        response.status(200).send(version);
+
+    });
+    response.status(200).send({"message":"all is up and running."});
 
 });
 
